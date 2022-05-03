fd1fc03f36d15f0d02726301e21232041112383c
Vinicin1101 <vserafim_o@outlook.com>
2022-05-03 16:31:34 -0300

    herois

--- introducao-nuxt/pages/index.vue
+++ introducao-nuxt/pages/index.vue
@@ -1,9 +1,51 @@
 <template>
-  <Tutorial/>
+  <div class="main">
+    <h1>{{ title }}</h1>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Poder</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="item.id" v-for="item in herois">
+          <td>{{item.nome}}</td>
+          <td>{{item.poder}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  export default {
+    data: function(){
+      return{
+        title: "Tabela de personagens DC",
+        herois: [
+          {id: 0, nome: "Flash", poder: (Math.random() * 100).toFixed(0)},
+          {id: 1, nome: "Batman", poder: (Math.random() * 100).toFixed(0)},
+          {id: 2, nome: "Dr. Manhattan", poder: (Math.random() * 100).toFixed(0)},
+          {id: 2, nome: "John Constantine", poder: (Math.random() * 100).toFixed(0)},
+        ]
+      }
+    }
+  }
 </script>
+
+<style>
+  table * {
+    border: 1px solid #000;
+    border-collapse: collapse;
+    padding: 10px 15px;
+  }
+  table th{
+    background-color: #000;
+    color: #fff;
+  }
+
+  table th:first-child{
+    border-right-color: #fff;
+  }
+</style>
